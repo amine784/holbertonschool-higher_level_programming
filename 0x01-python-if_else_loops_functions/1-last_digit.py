9479e27adac78f9c49c97f0f7dd772e8fda96be4
amine784 <1066@holbertonschool.com>
2019-12-10 14:49:01 +0100

    holbo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number >= 0:
     l = number % 10
 st = "and is less than 6 and not 0"
 if number < 0:
-    l = number * (-1) % 10
+    l = number % (10 * (-1))
 if l == 0:
     print("Last digit of {:d} is {:d} and is 0".format(number, l))
 elif l > 5:
