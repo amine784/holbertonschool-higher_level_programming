c426303ba919fdeb3fdf3509038a679cc78b7e31
amine784 <1066@holbertonschool.com>
2019-12-10 14:38:31 +0100

    holbo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l = number % 10
+if number >= 0:
+    l = number % 10
 st = "and is less than 6 and not 0"
 if number < 0:
     l = number * (-1) % 10
