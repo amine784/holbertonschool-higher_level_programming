dbc3cf99c68876e387677df9ac7abd621045fcc8
amine784 <1066@holbertonschool.com>
2019-12-10 17:05:43 +0100

    holberton

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
+    a = 97
+    z = 122
+    if ord(c) >= a and ord(c) <= z:
         return(True)
     else:
         return(False)
