218d54fef8fb60dd19c68d0420663405f9264a71
amine784 <1066@holbertonschool.com>
2019-12-12 20:24:19 +0100

    holberton

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-'''
 if __name__ == "__main__":
-'''
-import hidden_4
-for i in dir(hidden_4):
-    delim = "__"
-    if i[:2] != delim:
-        print(i)
+    import hidden_4
+    for i in dir(hidden_4):
+        delim = "__"
+        if i[:2] != delim:
+            print(i)
