be85a8899130f0a0b33eaee6ede86f2b5a6c7fc5
amine784 <1066@holbertonschool.com>
2019-12-12 15:44:10 +0100

    holberton

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+if __name__ == "__main__":
+'''
+import os
+import sys
+l = len(sys.argv)
+if l == 1:
+    print("{:d} argument.".format(l-1))
+elif l > 1:
+    print("{:d} argument:".format(l-1))
+l = len(sys.argv)
+for x in range(1, l):
+    print("{:d}: {:s}".format(x, (sys.argv[x])))
