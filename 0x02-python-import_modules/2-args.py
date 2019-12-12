6fe55b3fa4fa428a06b6d734b00dc1285d937a6f
amine784 <1066@holbertonschool.com>
2019-12-12 16:36:34 +0100

    holberton

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,10 @@ l = len(sys.argv)
 if l == 1:
     print("{:d} argument.".format(l-1))
 elif l > 1:
-    print("{:d} argument:".format(l-1))
+    if l == 2:
+        print("{:d} argument:".format(l-1))
+    else:
+        print("{:d} arguments:".format(l-1))
 l = len(sys.argv)
 for x in range(1, l):
     print("{:d}: {:s}".format(x, (sys.argv[x])))
