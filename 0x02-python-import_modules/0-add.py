e8f5480bd502b57de4a0d82ab243578c406cf22a
amine784 <1066@holbertonschool.com>
2019-12-12 14:40:31 +0100

    holly

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ if __name__ == "__main__":
     import add_0
     a = 1
     b = 2
-    c = add_0.add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, c))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
