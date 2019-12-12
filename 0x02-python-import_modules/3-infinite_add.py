1735603c3fc353bb3369a014294933c430ca9624
amine784 <1066@holbertonschool.com>
2019-12-12 17:22:10 +0100

    holberton

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-'''
-if __name__ == "__main__":
-'''
 import os
 import sys
-l = len(sys.argv)
-s = 0
-for x in range(1, l):
-    arg = (sys.argv[x])
-    s += int(arg)
+if __name__ == "__main__":
+    l = len(sys.argv)
+    s = 0
+    for x in range(1, l):
+        arg = (sys.argv[x])
+        s += int(arg)
 print("{:d}".format(s))
