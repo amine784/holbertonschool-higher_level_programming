3bd7125d0548b5f268fc99fe3849354c6d583ce0
amine784 <1066@holbertonschool.com>
2019-12-12 15:53:58 +0100

    holberton

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-'''
-if __name__ == "__main__":
-'''
 import os
 import sys
 l = len(sys.argv)
 if l == 1:
     print("{:d} argument.".format(l-1))
-elif l > 1:
+if l == 2:
+    print("{:d} argument:".format(l-1))
+elif l > 2:
     print("{:d} arguments:".format(l-1))
 l = len(sys.argv)
 for x in range(1, l):
