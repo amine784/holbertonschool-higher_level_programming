4e27670e50275f88babef1beddec63d9252e6585
amine784 <1066@holbertonschool.com>
2019-12-17 13:11:46 +0100

    hol

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+def element_at(my_list, idx):
     for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+        if idx > i or mylist[i] < 0:
+            return
+        return(mylist[idx])
