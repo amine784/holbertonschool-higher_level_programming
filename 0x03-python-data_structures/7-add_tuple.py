77b31758839f25e42def6df0d9a9b82ce6f9a39f
amine784 <1066@holbertonschool.com>
2019-12-17 18:23:54 +0100

    hgr

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if tuple_a == 0:
+        return(None)
+    if tuple_b == 0:
+        return(None)
+    x = tuple_a + ('0', '0')
+    y = tuple_b + ('0', '0')
+    a = int(x[0])
+    b = int(y[0])
+    s = a + b
+    c = int(x[1])
+    d = int(y[1])
+    s1 = c + d
+    k = s, s1
+    return(k)
