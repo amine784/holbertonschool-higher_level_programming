8795b8148c81130c382a3e6c8944920e83d50dc7
amine784 <1066@holbertonschool.com>
2019-12-19 19:51:49 +0100

    holberto

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for i in list(a_dictionary.keys()):
+        if a_dictionary.keys() == value:
+            a_dictionary.pop(key,)
+    return(a_dictionary)
