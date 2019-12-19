db513f402c2f91ff97a1888dedf5257ec3b1a8d2
amine784 <1066@holbertonschool.com>
2019-12-19 12:26:07 +0100

    new

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     liste = []
-    for i in my_list:
-        if i != search:
-            liste.append(i)
+    l = len(my_list)
+    for i in range(l):
+        if my_list[i] != search:
+            liste.append(my_list[i])
         else:
             liste.append(replace)
     return(liste)
