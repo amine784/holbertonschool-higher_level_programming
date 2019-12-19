cbb0f8825ce9ec165e08974d8f42d2e3166456a5
amine784 <1066@holbertonschool.com>
2019-12-19 15:48:25 +0100

    rtgfty

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,5 +4,5 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
     else:
         a_dictionary[key] = value
-    return (a_dictionary)
+    return(a_dictionary)
     
