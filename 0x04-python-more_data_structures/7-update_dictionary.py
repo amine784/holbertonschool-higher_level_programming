1f8327fc732ac8aa6719f5b63b021a6a507d175f
amine784 <1066@holbertonschool.com>
2019-12-19 15:21:22 +0100

    frtder

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key not in a_dictionary:
+        a_dectionary[key] = value
+    else:
+        a_dectionary[key] = value
+    return (a_dictionary)
+    
