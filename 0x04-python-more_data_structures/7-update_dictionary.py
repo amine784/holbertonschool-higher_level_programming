ea325060262b23a3cc99cb2c707fad8f6f107acf
amine784 <1066@holbertonschool.com>
2019-12-19 15:23:44 +0100

    reality

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
-        a_dectionary[key] = value
+        a_dictionary[key] = value
     else:
-        a_dectionary[key] = value
+        a_dictionary[key] = value
     return (a_dictionary)
     
