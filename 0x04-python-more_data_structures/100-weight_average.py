b53f11d192f2484e9c6c92091579cb207b0f3a90
amine784 <1066@holbertonschool.com>
2019-12-22 19:04:14 +0100

    final

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None or my_list == 0:
+    if my_list is None or len(my_list) == 0:
         return(0)
     tab = []
     for i in my_list:
