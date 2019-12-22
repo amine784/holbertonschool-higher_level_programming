369b5866b8d4ff0e233072926a8a321a7ec0012b
amine784 <1066@holbertonschool.com>
2019-12-22 18:57:44 +0100

    hoberto

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None or my_list == 0:
+        return(0)
+    tab = []
+    for i in my_list:
+        tab.append(i)
+    sub = 0
+    mul = 0
+    somme = 0.0
+    l = len(tab)
+    for i in range(0, l):
+        sub += tab[i][1]
+        mul = tab[i][1] * tab[i][0]
+        somme += mul
+    somme /= sub
+    return(somme)
