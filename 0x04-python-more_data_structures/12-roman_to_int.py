407cc65ec3fc9eab6cb67e34440a16896a05831a
amine784 <1066@holbertonschool.com>
2019-12-22 16:34:51 +0100

    ta3betnipfff

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
-        return(None)
+        return(0)
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     liste = []
     for r in roman_string:
