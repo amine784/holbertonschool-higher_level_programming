521904a8dcf9546977583697d03ea48538415adf
amine784 <1066@holbertonschool.com>
2019-12-22 16:23:45 +0100

    jesp

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,20 +2,23 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return(None)
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     liste = []
     for r in roman_string:
         for i in roman:
             if r == i:
                 x = roman.get(i)
                 liste.append(x)
-    l = len(liste)
-    vrai = []
-    a = 0
+    l = (len(liste))
+    s = 0
     for i in range(len(liste) - 1):
+        if liste[i] < liste[i+1]:
+            liste[i] = liste[i + 1] - liste[i]
+            liste[i+1] = 0
+    for i in range(len(liste)-1):
         if liste[i] < liste[i + 1]:
             liste[i] = liste[i + 1] - liste[i]
             liste[i + 1] = 0
-    for i in liste:
-        a += i
-    return(a)
+    for st in liste:
+        s = s + st
+    return(s)
