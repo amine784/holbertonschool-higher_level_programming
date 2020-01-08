7a585f395e6cfb1b48faf69da509136bef515545
amine784 <1066@holbertonschool.com>
2020-01-08 18:24:34 +0100

    holberton

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        sys.stderr.write("Eceptionx: {}".format(TypeError))
+    except (TypeError, ValueError) as error:
+        sys.stderr.write("Exception: {}".format(error))
         return False
