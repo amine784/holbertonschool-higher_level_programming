cf9ca71a164c03e3f4f3121ae4186db7b4ebf753
amine784 <1066@holbertonschool.com>
2020-01-08 14:25:55 +0100

    jesr

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
     except:
         return False
-    return True
+    
