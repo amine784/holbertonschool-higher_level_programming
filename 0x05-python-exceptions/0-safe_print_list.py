8e3a9e85a06a07bd99c5ef16c1d3d117704ff238
amine784 <1066@holbertonschool.com>
2020-01-08 13:50:16 +0100

    leao

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     len = 0
     for i in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print(my_list[i], end="")
             len += 1
         except:
             continue
