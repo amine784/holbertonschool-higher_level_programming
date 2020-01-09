504c530395de09424a418ffd4add82f4317b147d
amine784 <1066@holbertonschool.com>
2020-01-09 15:28:05 +0100

    secpush

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    '''define a neww class called square'''
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''class method that calculate the area of square'''
+        return(self.__size * self.__size)
