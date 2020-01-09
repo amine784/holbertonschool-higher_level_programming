ddbfeeb99c9dad8fc6a78ad664a87dcf3e46e497
amine784 <1066@holbertonschool.com>
2020-01-09 17:36:43 +0100

    der functi

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+class Square:
+    ''' define a neww class called square '''
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''class method that calculate the area of square'''
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        '''function to print'''
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
+        if self.__size == 0:
+            print("")
