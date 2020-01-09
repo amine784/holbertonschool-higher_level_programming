be6cadc05e54bb75680395e87860024c1352eea4
amine784 <1066@holbertonschool.com>
2020-01-09 15:16:57 +0100

    holberto

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    '''define a neww class called square'''
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
