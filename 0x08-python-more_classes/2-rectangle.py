9a784bd7b8d1ba61951b52491b3c2abc37ca9f17
amine784 <1066@holbertonschool.com>
2020-01-17 16:36:29 +0100

    confr

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     def method
     '''
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
