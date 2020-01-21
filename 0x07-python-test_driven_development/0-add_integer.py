50e6650e98a5a84bc6ed7ace7411aad4600e240b
amine784 <1066@holbertonschool.com>
2020-01-21 07:31:44 +0100

    holberto

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,5 +18,10 @@ def add_integer(a, b=98):
         raise TypeError(messageErrorA)
     if y is not int and y is not float:
         raise TypeError(messageErrorB)
+    x = a + b
+    if x < 0:
+        x = x * -1
+    if x == float("inf"):
+        raise ValueError("overflow")
     else:
         return(int(a) + int(b))
