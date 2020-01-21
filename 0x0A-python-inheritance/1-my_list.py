ea732fed5ae2738cb3d25c1d326ece42d3cf7ad5
amine784 <1066@holbertonschool.com>
2020-01-21 15:22:06 +0100

    next

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+return list of all object attribute
+'''
+
+
+class MyList(list):
+    '''def lookyp'''
+    def print_sorted(self):
+        '''def sorted list'''
+        print(sorted(self))
