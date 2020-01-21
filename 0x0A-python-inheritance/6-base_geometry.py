539159f6822e5314277b36a1ded2cb8250fc2261
amine784 <1066@holbertonschool.com>
2020-01-21 16:51:55 +0100

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+return subclass or not
+'''
+
+
+class BaseGeometry:
+    '''class BaseGeometry'''
+    def area(self):
+        '''method'''
+        raise Exception("area() is not implemented")
