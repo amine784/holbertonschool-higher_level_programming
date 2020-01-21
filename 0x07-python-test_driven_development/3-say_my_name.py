a309f85c3e16eb2aac247c2eb6a389c8ffb40330
amine784 <1066@holbertonschool.com>
2020-01-21 07:51:05 +0100

    amine

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,7 +10,7 @@ def say_my_name(first_name, last_name=""):
     '''
     method to print name
     '''
-    if type(first_name) is not str:
+    if type(first_name) is not str or len(first_name) == 0:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
