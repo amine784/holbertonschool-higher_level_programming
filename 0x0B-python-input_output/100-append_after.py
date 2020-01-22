8b128c377a52a1bccc42a2f18e9378d15ed98001
amine784 <1066@holbertonschool.com>
2020-01-22 18:13:56 +0100

    amine

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,8 +8,3 @@ def append_after(filename="", search_string="", new_string=""):
     '''def append after'''
     with open(filename, "r", encoding='utf-8') as File:
         line = File.readlines()
-    with open(filename, "w", encoding='utf-8') as File:
-        for i in line:
-            File.write(i)
-            if search_string in line:
-                File.write(new_string)
