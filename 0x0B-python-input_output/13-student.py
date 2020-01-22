f716ff5ce9c232dd0b6b7b46d978b8f31e4beef6
amine784 <1066@holbertonschool.com>
2020-01-22 18:20:49 +0100

    hobert

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+read file
+'''
+
+
+class Student:
+    '''function to read file'''
+    def __init__(self, first_name, last_name, age):
+        '''init function'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''function to jason'''
+        return(self.__dict__)
