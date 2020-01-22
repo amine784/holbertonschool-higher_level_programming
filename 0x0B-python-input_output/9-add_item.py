c095bf51d99295dea6353528d752ff08a6cfd405
amine784 <1066@holbertonschool.com>
2020-01-22 18:06:02 +0100

    holberton

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -11,7 +11,9 @@ ld_json = __import__('8-load_from_json_file').load_from_json_file
 liste = []
 if not os.path.exists("./add_item.json"):
     sv_json(liste, "add_item.json")
+else:
+    continue
 liste = ld_json("add_item.json")
-for i in sys.argv[1:]:
+for i in sys.argv[0:-1]:
     liste.append(i)
     sv_json(liste, "add_item.json")
