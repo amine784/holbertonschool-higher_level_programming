9968a87fc6cdff6b7c42b0172410128209701664
amine784 <1066@holbertonschool.com>
2020-01-22 20:59:07 +0100

    amine

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -6,12 +6,15 @@ import json
 import sys
 import os
 
+liste = []
 sv_json = __import__('7-save_to_json_file').save_to_json_file
 ld_json = __import__('8-load_from_json_file').load_from_json_file
-liste = []
-if not os.path.exists("./add_item.json"):
-    sv_json(liste, "add_item.json")
-liste = ld_json("add_item.json")
-for i in sys.argv[1:]:
+n = "./add_item.json"
+x = "./add_item.json"
+if not os.path.exists(n):
+    sv_json(liste, x)
+liste = ld_json(x)
+syst = sys.argv[1:]
+for i in syst:
     liste.append(i)
-    sv_json(liste, "add_item.json")
+    sv_json(liste, x)
