b4092f6de463c4b74eb8353aa369a5cbc1c327ce
amine784 <1066@holbertonschool.com>
2020-01-27 19:12:19 +0100

    holberto

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,24 +23,23 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        '''update att and key'''
-        if(args is not None) and (len(args) > 0):
-            for i in range(0, len(args)):
-                for arg in args:
-                    if j == 0:
-                        self.id = arg
-                    elif j == 1:
-                        self.size = arg
-                    elif j == 2:
-                        self.x = arg
-                    elif j == 3:
-                        self.y = arg
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
     def to_dictionary(self):
         '''convert to dictionary'''
         return({"id": self.id, "size": self.size,
                 "x": self.x, "y": self.y})
+
+    def update(self, *args, **kwargs):
+        '''update square attributes'''
+        if args:
+            for j, arg in enumerate(args):
+                if j == 0:
+                    self.id = arg
+                if j == 1:
+                    self.size = arg
+                if j == 2:
+                    self.x = arg
+                if j == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
