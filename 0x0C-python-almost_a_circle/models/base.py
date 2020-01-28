0d8088cc7189c27c3f26171e04aec939f710cb91
amine784 <1066@holbertonschool.com>
2020-01-28 17:02:18 +0100

    holberto

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''
+create base class
+'''
+import json
+
+
+class Base:
+    '''create new classe named base'''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''method of class'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
