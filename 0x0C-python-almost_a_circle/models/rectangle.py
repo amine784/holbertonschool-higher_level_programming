7194e66e63059bd9a0906cf29535ef8398faa406
amine784 <1066@holbertonschool.com>
2020-01-28 17:50:11 +0100

    holberto

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,3 +106,9 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        '''convert to dictionary'''
+        return({"id": self.id, "width": self.__width,
+                "height": self.__height,
+                "x": self.__x, "y": self.__y})
