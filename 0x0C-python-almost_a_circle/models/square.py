8586f8e9cc8da8e0bc4dd27b584f4f78ce73a12b
amine784 <1066@holbertonschool.com>
2020-01-28 18:03:24 +0100

    recc

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,32 @@
-f
+#!/usr/bin/python3
+'''new class square'''
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''new class that inherite from Base'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''new methode'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''str def'''
+        return("[Square] ({}) {}/{} - {}"
+               .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        '''self size'''
+        return(self.width)
+
+    @size.setter
+    def size(self, value):
+        ''' size'''
+        self.width = value
+        self.height = value
+
+    def to_dictionary(self):
+        '''convert to dictionary'''
+        return({"id": self.id, "size": self.size,
+                "x": self.x, "y": self.y})
