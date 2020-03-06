27c5ffc8886ed088c808861097c735de0fe85ef5
amine784 <1066@holbertonschool.com>
2020-03-06 19:29:35 +0100

    new

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---create database if not exsist
+-- create database if not exsist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---rm database
+-- rm database
 DROP DATABASE IF EXISTS hbtn_0c_0;
