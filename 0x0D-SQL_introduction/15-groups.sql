7450c98aad87caa18eed318d385da50e6a707a68
amine784 <1066@holbertonschool.com>
2020-03-06 20:59:41 +0100

    new push

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- list number
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- no link
+SELECT score, name FROM second_table WHERE NAME IS NOT NULL ORDER BY score DESC
