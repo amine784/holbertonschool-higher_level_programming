2e2e7ee732e0d47c3d6a3b542fc9afeaa9ee2ae6
amine784 <1066@holbertonschool.com>
2020-03-10 00:02:02 +0100

    last push

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
---ists all the cities of California that can be found in the database
-SELECT id,name FROM cities WHERE cities.state_id=1 ORDER BY cities.id;
+-- lists all the cities of California that can be found in the database
+SELECT id,name FROM cities WHERE state_id=1 ORDER BY id ASC;
