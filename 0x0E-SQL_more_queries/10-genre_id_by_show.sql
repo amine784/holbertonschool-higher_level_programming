60fdf785672f61b7c22562a4c49c8f62981076b1
amine784 <1066@holbertonschool.com>
2020-03-11 03:33:51 +0100

    holberto

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- cript that lists all shows contained in
 -- hbtn_0d_tvshows that have at least one genre linked.
-SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows,tv_shows_genres WHERE tv_show_genres.genres.show_id=tv_shows_id ORDER BY  tv_shows.title,tv_show_genres.genre_id;
+SELECT tv_shows.title,tv_show_genres.genre_id FROM
