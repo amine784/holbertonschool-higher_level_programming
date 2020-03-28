95b3a7382e6d3d0c2c6befb29f73a24e27a1863b
amine784 <1066@holbertonschool.com>
2020-03-28 14:13:32 +0100

    holberto

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            user=sys.argv[1],
                            passwd=sys.argv[2],
                            db=sys.argv[3])
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{:s}'".format(
+    query = "SELECT * FROM states WHERE name = '{}'".format(
         sys.argv[4])
     cur = data.cursor()
     cur.execute(query)
