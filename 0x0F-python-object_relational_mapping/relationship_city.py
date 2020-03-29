69411e1dc2a3b7faf3464a4ae6e021c68f05597a
amine784 <1066@holbertonschool.com>
2020-03-29 19:53:54 +0100

    holberto

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' script that def city'''
+
+
+import sys
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class City(Base):
+    '''def city'''
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
