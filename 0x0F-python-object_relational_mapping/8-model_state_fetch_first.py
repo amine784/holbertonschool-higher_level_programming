818e0a28c3f21c0a4394e4ca27860363672f2175
amine784 <1066@holbertonschool.com>
2020-03-29 12:03:01 +0100

    holbert

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' script that def state'''
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from sqlalchemy.orm import Session
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    inst = session.query(State).order_by(State.id).first()
+    if inst:
+        print("{}: {}".format(inst.id, inst.name))
+    print("Nothing")
