95be6150c3c8527e397ad6850e20a5008b3c290a
amine784 <1066@holbertonschool.com>
2020-04-09 12:44:10 +0100

    holberto

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Bash script that sends a request to a URL passed as an
-#argument, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" $1
+# Bash script that sends a request to a URL passed as
+curl -o /dev/null -w "%{http_code}" -s "$1"
