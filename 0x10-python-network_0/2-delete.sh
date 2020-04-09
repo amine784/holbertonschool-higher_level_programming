01458ed217084b93377170ffb407ff2441e14b93
amine784 <1066@holbertonschool.com>
2020-04-09 12:20:26 +0100

    hobert

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that sends a DELETE request to the URL passed as
-# the first argument and displays the body of the response
+# Bash script that sends a DELETE request to the URL pa
 curl -X DELETE -s "$1"
