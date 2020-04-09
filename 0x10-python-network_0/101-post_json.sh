04549371c09afcd65d2c67ea999630b75f0e5336
amine784 <1066@holbertonschool.com>
2020-04-09 12:56:46 +0100

    holberto

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed
-curl -H "content-Type: application/json" -s "$1" @"$2"
+curl -H "content-Type: application/json" -s "$1" -X POST -d @"$2"
