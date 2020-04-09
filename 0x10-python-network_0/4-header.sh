b1c2c1d571ece927fbb7484b0686de5d0b1d00f8
amine784 <1066@holbertonschool.com>
2020-04-09 12:35:37 +0100

    holberto

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#script that takes in a URL as an argument, sends a GET 
-#request to the URL, and displays the body of the response
-curl -sH "X-HolbertonSchool-User-Id:98" $1
+#script that takes in a URL as an argument, sends a GET request
+curl -H "X-HolbertonSchool-User-Id:98" -s"$1"
