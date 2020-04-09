53580232ab3d0799be9e467653e8bbee83b6d31b
amine784 <1066@holbertonschool.com>
2020-04-09 12:40:29 +0100

    big push

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#script that takes in a URL as an argument, sends a GET request
-curl -H "X-HolbertonSchool-User-Id: 98" -s"$1"
+# script that takes in a URL as an argument, sends a GET request
+curl -H "X-HolbertonSchool-User-Id: 98" -s "$1"
