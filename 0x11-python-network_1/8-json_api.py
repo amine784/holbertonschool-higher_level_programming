6ec52e361a500702095eb4d65b739002383e13df
amine784 <1066@holbertonschool.com>
2020-04-15 20:11:40 +0100

    finly

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-'''script tosends a POST request to the passed URL with the email
-'''
+'''script tosends a POST request to the passed URL with the email'''
 from sys import argv
 import requests
 if __name__ == "__main__":
-    if (len(argv) == 1):
-        q = ""
-    else:
+    q = ''
+    if (len(argv) == 2):
         q = argv[1]
-    jason = (requests.post("http://0.0.0.0:5000/search_user")).json()
-    if len(jason) == 0:
-        print("{}".format("No result"))
-    if (jason):
-        print("[{}] {}".format(jason.get("id"), jason.get("name")))
-    else:
-        print("{}".format("Not a valid JASON"))
+    try:
+        s = (requests.post("http://0.0.0.0:5000/search_user", {"q": q})).json()
+        if not s:
+            print("{}".format("No result"))
+        else:
+            print("[{}] {}".format(s.get("id"), s.get("name")))
+    except:
+        print("{}".format("Not a valid JSON"))
