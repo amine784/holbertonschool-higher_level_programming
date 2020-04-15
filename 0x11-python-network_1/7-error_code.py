be26801a879377dddb8751eca8cc18ca910e6bd6
amine784 <1066@holbertonschool.com>
2020-04-15 18:21:41 +0100

    holberton

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''script tosends a POST request to the passed URL with the email
+'''
+from sys import argv
+import requests
+if __name__ == "__main__":
+    if ((requests.get(argv[1]).status_code >= 400)):
+        print("Error code: {}".format(requests.get(argv[1]).status_code))
+    print((requests.get(argv[1])).text)
