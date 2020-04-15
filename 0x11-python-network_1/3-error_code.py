d25cc2d96c6d2a12602a9c9747ff1deec1d57465
amine784 <1066@holbertonschool.com>
2020-04-15 15:11:48 +0100

    holberto

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''ends a request to the URL and displays the value of the X-Request-Id
+'''
+import urllib.request
+import sys
+if __name__ == "__main__":
+        try:
+            with request.urlopen(sys.argv[1]) as req:
+                    print(req.read().decode("utf-8"))
+        except error.HTTPError as req:
+            print("Error code: {}"format(req.code))
