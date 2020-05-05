e5c51c9a85ad27b49c4105029779f8d63355f9cb
amine784 <1066@holbertonschool.com>
2020-05-05 15:52:11 +0100

    holbert jevink

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const n = parseInt(nbre, 10);
 const l = process.argv.sort();
 const max = l[l.length - 1];
 if (n <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
   console.log(max - 1);
 }
