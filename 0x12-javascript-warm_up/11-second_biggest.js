f23dfba31328e75430bdf0abee987f57dd19fae1
amine784 <1066@holbertonschool.com>
2020-05-05 15:54:12 +0100

    holberto

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const nbre = process.argv.length;
-const n = parseInt(nbre, 10);
-const l = process.argv.sort();
-const max = l[l.length - 1];
-if (n <= 3) {
-  console.log('0');
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-  console.log(max - 1);
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
