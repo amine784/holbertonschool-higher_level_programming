1e122c47d6972dadc6468c2ff7649193e6c31872
amine784 <1066@holbertonschool.com>
2020-05-05 13:05:40 +0100

    holberto

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const args = process.argv.length;
 if (args <= 2) {
   console.log('No argument');
-} else if (args > 2) {
+} else if (args === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
