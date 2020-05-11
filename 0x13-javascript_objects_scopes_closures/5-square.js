a7d1fc83f791aab5e53bf7f338d55f93d033da17
amine784 <1066@holbertonschool.com>
2020-05-11 15:52:39 +0100

    holberto

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && Number.isInteger(w) !== false &&
+      h > 0 && Number.isInteger(h) !== false) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
