adf70d22a6b152ccfb8f00f17b2f3fa7678b350a
amine784 <1066@holbertonschool.com>
2020-05-12 21:49:04 +0100

    new pushrt

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,11 @@ const request = require('request');
 request(process.argv[2], function (err, response, body) {
   if (err) { console.log(err); }
   let m = 0;
-  let c;
+  let c, h;
   const data = JSON.parse(body).results;
   for (c of data) {
-    for (const h of c.characters) {
-      if (h.includes('/18/')) { m += 1; }
+    for (h of c.characters) {
+      if (h.includes('18')) { m++; }
     }
   }
   console.log(m);
