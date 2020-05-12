a16fce2a1b4ebccc72f18168c6a2f973b42413ea
amine784 <1066@holbertonschool.com>
2020-05-12 14:03:22 +0100

    new push

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const req = require('request');
-req('https://swapi-api.hbtn.io/api/' + process.argv[2], (err, response, body) => { if (err) { console.log(err); } else { console.log(JSON.parse(response.body).title); } });
+const request = require('request');
+request(`http://swapi.co/api/films/${process.argv[2]}`, function (error, response, body) {
+  error && console.log(error);
+  console.log(JSON.parse(body).title);
+});
