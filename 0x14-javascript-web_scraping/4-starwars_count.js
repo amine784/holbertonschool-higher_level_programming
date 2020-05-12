dd00159ac6f04d43fc4ee3fdf9cbbf67fedc5333
amine784 <1066@holbertonschool.com>
2020-05-12 22:04:33 +0100

    new push all

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (err, response, body) { if (err) { console.log(err); }
+require('request')(process.argv[2], function (err, res, body) {
+  if (err) { console.log(err); }
   let m = 0;
   let c, h;
   const data = JSON.parse(body).results;
