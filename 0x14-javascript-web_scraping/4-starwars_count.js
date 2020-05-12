aab603a12693af087858260da2ebec037759d2dd
amine784 <1066@holbertonschool.com>
2020-05-12 22:37:48 +0100

    holberto

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-require('request')(process.argv[2], (err, res, body) => {
+require('request')(process.argv[2], { json: true }, (err, res, body) => {
   if (err) console.log(err);
   else {
     let m = 0;
     let c, h;
-    const data = JSON.parse(body).results;
+    const data = body.results;
     for (c of data) {
       for (h of c.characters) {
         if (h.includes('18')) { m++; }
