ababd211d34ce90cb74e028492ecf4c8bb5173b6
amine784 <1066@holbertonschool.com>
2020-05-12 22:25:22 +0100

    last pushbn

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
-require('request')(process.argv[2], function (err, res, body) {
-  if (err) { console.log(err); }
-  let m = 0;
-  let c, h;
-  const data = JSON.parse(body).results;
-  for (c of data) {
-    for (h of c.characters) {
-      if (h.includes('18')) { m++; }
+require('request')(process.argv[2], (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    let m = 0;
+    let c, h;
+    const data = JSON.parse(body).results;
+    for (c of data) {
+      for (h of c.characters) {
+        if (h.includes('18')) { m++; }
+      }
     }
+    console.log(m);
   }
-  console.log(m);
 });
