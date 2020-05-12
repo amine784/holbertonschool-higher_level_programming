39eed531d7f853cfd0836a622bf99f834db3698b
amine784 <1066@holbertonschool.com>
2020-05-12 23:41:53 +0100

    new prtjh

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,6 +3,8 @@ require('request')(process.argv[2], (err, res, body) => {
   if (err) console.log(err);
   else {
     const fil = require('fs');
-    fil.writeFile(process.argv[3], body, 'utf-8');
+    fil.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) console.log(error);
+    });
   }
 });
