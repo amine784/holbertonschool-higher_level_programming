73045a983cb03fd042e08fbd3d00bb16ee4e785d
amine784 <1066@holbertonschool.com>
2020-05-12 23:43:13 +0100

    holberto

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-require('request')(process.argv[2], (err, res, body) => {
-  if (err) console.log(err);
+require('request')(process.argv[2], (err, res, body) => { if (err) console.log(err);
   else {
     const fil = require('fs');
     fil.writeFile(process.argv[3], body, 'utf-8', (error) => {
