c3946292447c50f2d1427c5cf9881a8cfede50dc
amine784 <1066@holbertonschool.com>
2020-05-14 23:13:28 +0100

    holberto

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,6 @@
-const url = 'https://swapi-api.hbtn.io/api/films/?format=json'
-$.get(url, (data) =>
-{
-  for (let x = 0; i < data.results.length; x++) {
-    $("UL#list_movies").append("<li>" + data.results[x].title + "</li>");
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+$.get(url, (data) => {
+  for (let x = 0; x < data.results.length; x++) {
+    $('UL#list_movies').append('<li>' + data.results[x].title + '</li>');
   }
 });
